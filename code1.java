d951b0edf35aff05939f5a4cadb35e3cf8bd5b2e
SUMIYA129 <sumiyarahaman234@gmail.com>
2023-09-12 15:29:33 +0530

    final code

--- code1.java
+++ code1.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 interface Team
 {
  int add(int a, int b)
@@ -7,5 +8,19 @@ class TeamImpl implements Team{
     public int add(int a,int b)
     {
         return a+b;
+    }
+}
+class TeamLeader
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter two numbes to sum");
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        TeamImpl t1=new TeamImpl();
+        int sum=n1+n2;
+        t1.add(n1, n2);
+        System.out.println(sum);
     }
 }
